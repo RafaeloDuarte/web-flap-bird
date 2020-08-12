f9552d2c0aa66d65c215128baf1a2f0d45ec7b68
Rafa <rafaeloliveiraduarte@hotmail.com>
2020-08-11 23:37:29 -0300

    Inserindo primeiros obstaculos.

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const gameDisplay = document.querySelector('.game-container')
     const ground = document.querySelector('.ground')
     const bird = document.querySelector('.bird')
+    const obstacleUp = document.querySelector('.obstacleUp')
+    const obstacleDown = document.querySelector('.obstacleDown')
 
     let birdLeft = 220
     let birdBottom = 300
+    let obstacleLeft = 450
     let gravity = 2
 
     function startGame(){
-        birdBottom -= gravity
+        if(birdBottom > 0) birdBottom -= gravity
         bird.style.bottom = birdBottom + 'px'
         bird.style.left = birdLeft+'px'
+        obstacleMovement()
     }
     let timerId = setInterval(startGame, 20)
 
@@ -18,6 +22,13 @@ document.addEventListener('DOMContentLoaded', () => {
         if(e.keyCode === 32){
             jump()
         }
+    }
+
+    function obstacleMovement(){
+        if(obstacleLeft > -60) obstacleLeft -= gravity
+        else obstacleLeft = 450
+        obstacleUp.style.left = obstacleLeft+'px'
+        obstacleDown.style.left = obstacleLeft+'px'
     }
     
     function jump(){
