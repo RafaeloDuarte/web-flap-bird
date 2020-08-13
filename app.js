5a8b979609e028a37a0767f73f7c538b529cf8f2
RafaeloDuarte <rafaeloduarte@gmail.com>
2020-08-13 17:49:56 -0300

    Estilização dos canos, fundo e passaro.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gameDisplay = document.querySelector('.game-container')
-    const ground = document.querySelector('.ground')
     const bird = document.querySelector('.bird')
     const obstacleUp = document.querySelector('.obstacleUp')
     const obstacleDown = document.querySelector('.obstacleDown')
@@ -9,23 +8,34 @@ document.addEventListener('DOMContentLoaded', () => {
     let birdLeft = 220
     let birdBottom = 300
     let obstacleLeft = 400
+	let obstacleRigth = obstacleLeft+50
 	let groundBase = 500
     let gravity = 2
 	let score = 0
-			displayText.value = score
+	let timer = 0
+	let condition = true
 
     function startGame(){
         if(birdBottom > 0) birdBottom -= gravity
         bird.style.bottom = birdBottom + 'px'
         bird.style.left = birdLeft+'px'
 		birdBorder = birdLeft+50
+		obstacleRigth = obstacleLeft+50
+		
 		if((birdBorder === obstacleLeft && (birdBottom<200 || birdBottom>330))
 			|| (birdBorder === obstacleLeft+50 && birdBottom<200)
 			|| birdBottom === 50) {
 			reload()
 		}
-
-	
+		
+		if(birdLeft > obstacleLeft && condition && obstacleRigth > 0){
+			score++
+			condition = false
+		} else if(obstacleRigth < 0 && !condition) {
+			condition = true
+			obstacleRigth = 0
+		}
+		displayText.innerHTML = score
         obstacleMovement()
     }
     let timerId = setInterval(startGame, 20)
